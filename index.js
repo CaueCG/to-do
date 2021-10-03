56efe8107317e0d25fd443fff076ef0732ae555e
Cauê Gualberto <caue.gualberto00@gmail.com>
2021-10-02 22:10:08 -0300

    tentando localstorage

--- index.js
+++ index.js
@@ -4,40 +4,60 @@ const btnDelete = document.getElementsByClassName("btnDelete");
 
 let todo = [];
 
+const localStorageTodos = JSON.parse(localStorage.getItem("todo"));
+let todos = localStorage.getItem("todo") !== null ? localStorageTodos : [];
+
+const updateLocalStorage = () => {
+  localStorage.setItem("todo", JSON.stringify(todo));
+};
+
+/*try {
+  todo = JSON.parse(localStorage.getItem("todos")) || [];
+} catch {}*/
+
 formTodo.addEventListener("submit", (event) => {
   event.preventDefault();
   const inputTodo = document.getElementById("inputAdd");
   let value = inputTodo.value.trim();
 
   if (value) {
-    reloadNewTask(inputTodo.value);
-
     todo.push({
       name: inputTodo.value,
       isDone: false,
     });
-
+    // localStorage.setItem("todos", JSON.stringify(todo));
     inputTodo.value = "";
+    console.log(todo);
+    updateLocalStorage();
   } else {
     alert("Empty field, type something!");
   }
+
+  rechargeTodo();
 });
 
-function reloadNewTask(newTodo) {
-  listTodo.innerHTML += `   <div id="itemTodo${todo.length}" class="itemTodo">
-      <input type="text" value=${newTodo} readonly />
+function rechargeTodo() {
+  listTodo.innerHTML = "";
+  let currentTodo = "";
+
+  todo.forEach((element, index) => {
+    currentTodo += `   <div id="itemTodo${index}" class="itemTodo">
+      <input type="text" value="${element.name}" readonly />
       <div class="buttonsTodo">
-        <button class="btnEdit" id="btnEdit${todo.length}"><i class="fas fa-edit fa-lg"></i></button>
-        <button class="btnDelete" id="btnDelete${todo.length}"><i class="fas fa-trash-alt fa-lg"></i></button>
+        <button class="btnEdit" id="btnEdit${index}"><i class="fas fa-edit fa-lg"></i></button>
+        <button class="btnDelete" id="btnDelete${index}"><i class="fas fa-trash-alt fa-lg"></i></button>
       </div>
     </div>`;
+  });
+  listTodo.innerHTML += currentTodo;
+  currentTodo = "";
 }
 
-todo.forEach((element, index) => {
+/*todo.forEach((element, index) => {
   let bDelete = document.getElementById(`btnDelete${index}`);
   bDelete.addEventListener("click", () => {
     listTodo.removeChild(`itemTodo${index}`);
     todo.splice(index);
     console.log(todo);
   });
-});
+});*/
